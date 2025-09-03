e87eb0d8c86a0786267b1d7150da833a62f88104
DANI05132 <weroarmendariz123@gmail.com>
2025-09-03 11:22:49 -0700

    Create ej2.cs

---
+++ ej2/ej2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Arreglo
+{
+    private List<string> Nombres = new List<string>();
+
+    public void AgregarNombre(string nombre)
+    {
+        Nombres.Add(nombre);
+    }
+
+    public string ObtenerNombre(int posicion)
+    {
+        if (posicion >= 0 && posicion < Nombres.Count)
+            return Nombres[posicion];
+        else
+            return "Posición inválida";
+    }
+
+    public int Cantidad()
+    {
+        return Nombres.Count;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Arreglo Arr = new Arreglo();
+        string Nombre;
+        char Continuar;
+
+        do
+        {
+            Console.Write("Ingrese su nombre: ");
+            Nombre = Console.ReadLine();
+            Arr.AgregarNombre(Nombre);
+
+            int UltimaPos = Arr.Cantidad() - 1;
+            Console.WriteLine($"El nombre guardado en la posición {UltimaPos} es: {Arr.ObtenerNombre(UltimaPos)}");
+
+            Console.Write("Desea continuar? (S/N): ");
+            Continuar = Char.ToUpper(Console.ReadKey().KeyChar);
+            Console.WriteLine();
+
+        } while (Continuar == 'S');
+    }
+}
